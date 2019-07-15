3cbc2b22e80e254256df58bc6f7b9ecd28f721b1
leeyazhou <lee_yazhou@163.com>
2019-07-15 15:32:32 +0800

    add flower.showcase.simple

---
+++ flower.showcase.service/src/main/java/com/ly/train/flower/showcase/service/simple/UserServiceB.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright © 2019 同程艺龙 (zhihui.li@ly.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ly.train.flower.showcase.service.simple;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.ly.train.flower.common.annotation.FlowerService;
+import com.ly.train.flower.common.core.service.ServiceContext;
+import com.ly.train.flower.core.service.impl.AbstractService;
+import com.ly.train.flower.showcase.model.User;
+
+/**
+ * @author leeyazhou
+ */
+@FlowerService
+public class UserServiceB extends AbstractService<User, User> {
+  static final Logger logger = LoggerFactory.getLogger(UserServiceB.class);
+
+  @Override
+  public User doProcess(User message, ServiceContext context) throws Throwable {
+    logger.info("处理任务 : {}", message);
+    Integer age = message.getAge();
+    if (age == null) {
+      age = 0;
+    }
+    message.setAge(++age);
+    return message;
+  }
+
+}
---
+++ flower.showcase.simple/src/main/java/com/ly/train/flower/showcase/simple/SimpleSyncFlower.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright © 2019 同程艺龙 (zhihui.li@ly.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ly.train.flower.showcase.simple;
+
+import java.util.concurrent.TimeoutException;
+import com.ly.train.flower.core.akka.router.FlowRouter;
+import com.ly.train.flower.core.service.container.FlowerFactory;
+import com.ly.train.flower.core.service.container.ServiceFlow;
+import com.ly.train.flower.core.service.container.simple.SimpleFlowerFactory;
+import com.ly.train.flower.showcase.model.User;
+import com.ly.train.flower.showcase.service.EndService;
+import com.ly.train.flower.showcase.service.simple.UserServiceA;
+import com.ly.train.flower.showcase.service.simple.UserServiceB;
+
+/**
+ * @author leeyazhou
+ */
+public class SimpleSyncFlower {
+
+  public static void main(String[] args) throws TimeoutException {
+    FlowerFactory flowerFactory = new SimpleFlowerFactory("flower.yml");
+
+    final String flowName = "flower.simpleflow";
+
+    ServiceFlow serviceFlow = flowerFactory.getServiceFactory().getOrCreateServiceFlow(flowName);
+    serviceFlow.buildFlow(UserServiceA.class, UserServiceB.class);
+    serviceFlow.buildFlow(UserServiceB.class, EndService.class);
+    serviceFlow.build();
+
+    FlowRouter flowRouter = flowerFactory.getActorFactory().buildFlowRouter(flowName, 8);
+
+    User user = new User();
+    user.setName("simple flower");
+    user.setAge(0);
+    User result = flowRouter.syncCallService(user);
+    System.out.println("结果,age ：" + result.getAge());
+    
+    flowerFactory.stop();
+  }
+}

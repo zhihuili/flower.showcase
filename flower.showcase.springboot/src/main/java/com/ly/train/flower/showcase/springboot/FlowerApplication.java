78e729520160fdd962d68c8c5e15fd8c75feee41
leeyazhou <lee_yazhou@163.com>
2020-01-08 10:17:37 +0800

    update

--- flower.showcase.springboot/src/main/java/com/ly/train/flower/showcase/springboot/FlowerApplication.java
+++ flower.showcase.springboot/src/main/java/com/ly/train/flower/showcase/springboot/FlowerApplication.java
@@ -3,10 +3,13 @@ package com.ly.train.flower.showcase.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ly.train.flower.web.spring.context.FlowerComponentScan;
+
 /**
  * @author leeyazhou
  */
 @SpringBootApplication
+@FlowerComponentScan("com.ly.train.flower")
 public class FlowerApplication {
 
   public static void main(String[] args) {
